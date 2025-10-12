656a7751dff8e7fa8066b2bf5acc187af5ab6795
Pkhoa@280100 <69888512+Henri280100@users.noreply.github.com>
2025-10-12 23:29:27 +0700

    Add UI5 app (pir_ui5) as part of this repo

--- app/pr-ui5/dist/index.js
+++
@@ -1,2 +0,0 @@
-sap.ui.define(["sap/ui/core/ComponentContainer"],function(e){"use strict";new e({manifest:true,name:"sap.ui.prui5",height:"100%",async:true}).placeAt("content")});
-//# sourceMappingURL=index.js.map
